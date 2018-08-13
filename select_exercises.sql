3d528b7a74da2964102e246a143b740f5d4c5619
mkovar <mkovar@mkovar.local>
2018-08-13 10:45:03 -0500

    select exercise file, working with albums_seeder data

---
+++ select_exercises.sql
@@ -0,0 +1,20 @@
+USE codeup_test_db;
+-- INSERT INTO albums (artist, name, release_date, genre, sales)
+
+-- SELECT 'The name of all albums by Pink Floyd' AS 'Info';
+
+-- SELECT * FROM albums
+-- WHERE artist = 'Pink Floyd';
+
+-- SELECT release_date FROM albums
+-- WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+-- SELECT genre FROM albums
+-- WHERE name = 'Nevermind';
+
+-- SELECT * FROM albums
+-- WHERE release_dates
+-- BETWEEN 1990 AND 1999;
+
+SELECT * FROM albums
+WHERE sales < 20.0;
